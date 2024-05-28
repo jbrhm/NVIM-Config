3f3ed390fc69a9ec18ec7db89010bc8dbeb5fe42
jbrhm <jrabraham1904@gmail.com>
2024-05-28 14:24:32 -0400

    Update nvim tree

--- lua/john/plugins/nvim-tree.lua
+++ lua/john/plugins/nvim-tree.lua
@@ -1,9 +1,12 @@
 return 	{
 		"nvim-tree/nvim-tree.lua",
 		config = function()
-			local nvimtree = require("nvim-tree")
-			require("nvim-tree").setup()
-			local keymap = vim.keymap
-			keymap.set("n", "<C-N>", "<cmd>NvimTreeToggle<CR>", {desc = "Toggles the Tree"})
+		require("nvim-tree").setup({
+			filters = {
+				dotfiles = false,
+			},
+		})
+        local keymap = vim.keymap
+		keymap.set("n", "<C-N>", "<cmd>NvimTreeToggle<CR>", {desc = "Toggles the Tree"})
 		end,
 	}
