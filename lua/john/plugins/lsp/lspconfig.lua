8ad713ff02013decaee539beccdd613048187fe2
jbrhm <jrabraham1904@gmail.com>
2024-07-30 15:40:59 -0400

    CMake LSP

--- lua/john/plugins/lsp/lspconfig.lua
+++ lua/john/plugins/lsp/lspconfig.lua
@@ -33,5 +33,9 @@ return {
 	-- Python language server
 	lspconfig["pyright"].setup({})
 
+  -- CMake Language Server
+  -- https://github.com/regen100/cmake-language-server
+  lspconfig["cmake"].setup({})
+
   end,
 }
