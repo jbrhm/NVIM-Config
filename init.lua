4ba208e147c48473e47eb4ec36d6bf5fdead6a3c
jbrhm <jrabraham1904@gmail.com>
2025-01-28 09:00:08 -0500

    added clang format integration

--- init.lua
+++ init.lua
@@ -4,3 +4,13 @@ require("john.lazy")
 vim.cmd [[
   highlight Normal guibg=NONE
 ]]
+
+vim.cmd [[
+	if has('python')
+	  map <C-K> :pyf /home/john/.config/NVIM-Config/clang-format.py<cr>
+	  imap <C-K> <c-o>:pyf /home/john/.config/NVIM-Config/clang-format.py<cr>
+	elseif has('python3')
+	  map <C-K> :py3f /home/john/.config/NVIM-Config/clang-format.py<cr>
+	  imap <C-K> <c-o>:py3f /home/john/.config/NVIM-Config/clang-format.py<cr>
+	endif
+]]
