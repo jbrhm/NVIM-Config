e979e18aec53bde462b020ade90b4dc918ed8d03
jbrhm <jrabraham1904@gmail.com>
2025-01-27 21:07:11 -0500

    telescope commands

--- lua/john/plugins/telescope.lua
+++ lua/john/plugins/telescope.lua
@@ -27,8 +27,11 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+	keymap.set("n", "<leader>fp", "<cmd>Telescope planets<cr>", { desc = "Awesome" })
+	keymap.set("n", "<leader>fr", "<cmd>Telescope lsp_references<cr>", { desc = "Find references for a given function in lsp" })
+	keymap.set("n", "<leader>fm", "<cmd>Telescope man_pages<cr>", { desc = "Opens man pages" })
   end,
 }
